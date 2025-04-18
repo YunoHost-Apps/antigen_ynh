fb1c0e78fd2157f06878a458f1639ec59cebc122
Mistermasque <mistermasque@gmail.com>
2025-04-18 12:38:02 +0200

    Fix disabling zsh after removing app

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,33 +6,27 @@ ANTIGEN_DEFAULT_CONF="/etc/antigenrc"
 
 # Configure zsh as default login shell for all users
 _set_zsh_for_all_users() {
-    local zsh_bin
-    zsh_bin="$(which zsh)"
     if _is_zsh_for_all_users; then
         return 0
     fi
    
-    echo "map passwd loginShell \"${zsh_bin}\"" >> "/etc/nslcd.conf"
+    echo 'map passwd loginShell "/bin/zsh"' >> "/etc/nslcd.conf"
     return 0
 }
 
 # Restore default login shell for al users
 _unset_zsh_for_all_users() {
-    local zsh_bin
-    zsh_bin="$(which zsh)"
     if ! _is_zsh_for_all_users; then
         return 0
     fi
 
-    sed -i "#map passwd loginShell \"${zsh_bin}\"#d" "/etc/zsh/zshrc"
+    sed -i '/map passwd loginShell "\/bin\/zsh"/d' "/etc/nslcd.conf"
     return 0
 }
 
 # Check if zsh is enabled for all users
 _is_zsh_for_all_users() {
-    local zsh_bin
-    zsh_bin="$(which zsh)"
-    if grep -q "map passwd loginShell \"${zsh_bin}\"" "/etc/nslcd.conf"; then
+    if grep -q 'map passwd loginShell "/bin/zsh"' "/etc/nslcd.conf"; then
         return 0
     else
         return 1
@@ -45,7 +39,7 @@ _set_zsh_for_root_user() {
         return 0
     fi
    
-    chsh -s "$(which zsh)" root
+    chsh -s "/bin/zsh" root
     return 0
 }
 
@@ -55,13 +49,13 @@ _unset_zsh_for_root_user() {
         return 0
     fi
 
-    chsh -s "$(which bash)" root
+    chsh -s "/bin/bash" root
     return 0
 }
 
 # Check if zsh is enabled for root user
 _is_zsh_for_root_user() {
-    if grep "^root\:" /etc/passwd | grep -q "$(which zsh)"; then
+    if grep "^root\:" /etc/passwd | grep -q "/bin/zsh"; then
         return 0
     else
         return 1
@@ -87,6 +81,6 @@ _disable_loading_antigen_by_default() {
         return 0
     fi
 
-    sed -i "#source \"$ANTIGEN_LOADER_SCRIPT\"#d" "/etc/zsh/zshrc"
+    sed -i -s "#source \"$ANTIGEN_LOADER_SCRIPT\"##g" "/etc/zsh/zshrc"
     return 0
 }
